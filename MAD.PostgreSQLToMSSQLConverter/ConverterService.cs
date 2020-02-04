9324a7243af6b561b0b9858c2973932a83ab9ea8
maitlandmarshallapps <maitlandmarshallapps@gmail.com>
2020-02-04 14:41:50 +1000

    Basic conversion of PostgreSQL data and schema to MSSQL

---
+++ MAD.PostgreSQLToMSSQLConverter/ConverterService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAD.PostgreSQLToMSSQLConverter
+{
+    public class ConverterService
+    {
+        public async Task ConvertPostgreToMSSQL(string postgreConnectionString, string sqlConnectionString)
+        {
+
+        }
+    }
+}
